ef8faf567dc73a556c1d8d92ab2baafd6627bb87
swc4327 <swc4327@naver.com>
2021-08-21 19:40:29 +0900

    리뷰디테일 기능 추가

--- amuclient/app/src/main/java/com/awesome/amuclient/ui/main/view/ReviewDetailActivity.kt
+++ amuclient/app/src/main/java/com/awesome/amuclient/ui/main/view/ReviewDetailActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import com.awesome.amuclient.R
 import com.awesome.amuclient.data.model.Client
 import com.awesome.amuclient.data.model.ReviewList
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_review_detail.*
 
 class ReviewDetailActivity : AppCompatActivity() {
 
@@ -24,6 +26,24 @@ class ReviewDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_review_detail)
 
         client = intent.getParcelableExtra("client")
-        println(client?.nickname + "~~~~~~~~~~~~~~~~~~~~~")
+
+        initLayout()
+        initListener()
+    }
+
+    private fun initLayout() {
+        review_detail_client_name.text = client?.nickname
+        review_detail_client_count.text = "리뷰 수 " + client?.count.toString()
+        review_detail_client_point.text = "평균 평점 " + client?.point.toString()
+        Glide
+            .with(this)
+            .load(client?.image)
+            .into(profile_img)
+    }
+    
+    private fun initListener() {
+        close_review_detail.setOnClickListener {
+            finish()
+        }
     }
 }
